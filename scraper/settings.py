aa1510cd71e2d59708cace2a2ac9e69f88e4167c
Dane Lee <danelee931@gmail.com>
2026-09-07 16:26:17 -0700

    feat: corrected variable error

--- scraper/settings.py
+++ scraper/settings.py
@@ -59,7 +59,7 @@ DOWNLOAD_DELAY = 1
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    "books.pipelines.MongoPipeline": 300,
+    "scraper.pipelines.MongoPipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
