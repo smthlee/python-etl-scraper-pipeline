628f4ca6d499973dfcdf338c4766f73c272aba6b
Dane Lee <danelee931@gmail.com>
2026-09-07 15:49:41 -0700

    feat: added MongoDB connection details

--- scraper/settings.py
+++ scraper/settings.py
@@ -85,3 +85,6 @@ DOWNLOAD_DELAY = 1
 
 # Set settings whose default value is deprecated to a future-proof value
 FEED_EXPORT_ENCODING = "utf-8"
+
+MONGO_URI = "mongodb://localhost:27017"
+MONGO_DATABASE = "books_db"
