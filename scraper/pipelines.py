0188e3e05bcfdc21d4a6407aab020cf58389f037
Dane Lee <danelee931@gmail.com>
2026-09-15 17:59:37 -0700

    fix: updated code to use an upsert operati on to prevent the dropping of items

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -26,12 +26,15 @@ class MongoPipeline:
 
     def process_item(self, item, spider):
         item_id = self.compute_item_id(item)
-        if self.db[self.COLLECTION_NAME].find_one({"_id": item_id}):
-            raise DropItem(f"Duplicate item found: {item}")
-        else:
-            item["_id"] = item_id
-            self.db[self.COLLECTION_NAME].insert_one(ItemAdapter(item).asdict())
-            return item
+        item_dict = ItemAdapter(item).asdict()
+
+        self.db[self.COLLECTION_NAME].update_one(
+            filter={"_id": item_id},
+            update={"$set": item_dict},
+            upsert=True
+        )
+
+        return item
 
     def compute_item_id(self, item):
         url = item["url"]
